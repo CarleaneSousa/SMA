d4962834ce56aa04c8b4e918379e5825d49ab5a4
CarleaneSousa <carleane.1sousa@gmail.com>
2018-05-24 16:50:53 -0300

    Primeiro

--- app/src/main/java/com/example/marcelomonier/iisemanademeioambiente/MenuActivity.java
+++ app/src/main/java/com/example/marcelomonier/iisemanademeioambiente/MenuActivity.java
@@ -1,20 +1,27 @@
 package com.example.marcelomonier.iisemanademeioambiente;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MenuActivity extends AppCompatActivity {
 
-    ImageView logo, nome_logo, programacao_imagem, qrcode_imagem, local_imagem, inscricao_imagem;
+    ImageView logo, nome_logo, programacao_imagem, qrcode_imagem, qrcode_gerar, local_imagem, inscricao_imagem;
 
     TextView nome_programacao, nome_qrcode, nome_local, nome_inscricao;
+
 
 
     @Override
@@ -26,8 +33,8 @@ public class MenuActivity extends AppCompatActivity {
 
         programacao_imagem = (ImageView) findViewById(R.id.imageView6);
         qrcode_imagem = (ImageView) findViewById(R.id.imageView5);
-        local_imagem  = (ImageView) findViewById(R.id.imageView9);
-
+        qrcode_gerar = (ImageView) findViewById(R.id.imageView7);
+        local_imagem = (ImageView) findViewById(R.id.imageView9);
 
 
         programacao_imagem.setOnClickListener(new View.OnClickListener() {
@@ -37,7 +44,6 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-
 
 
         local_imagem.setOnClickListener(new View.OnClickListener() {
@@ -50,8 +56,28 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+        final Activity activity = this;
+        qrcode_imagem.setOnClickListener(new View.OnClickListener() {
+         @Override
+        public void onClick(View view) {
 
+             IntentIntegrator integrator = new IntentIntegrator(activity);
+             integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+             integrator.setPrompt("Scan");
+             integrator.setCameraId(0);
+             integrator.setBeepEnabled(false);
+             integrator.setBarcodeImageEnabled(false);
+             integrator.initiateScan();
 
+         }
+    });
 
+        qrcode_gerar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent rIntent = new Intent(MenuActivity.this, GerarQRcodeActivity.class);
+                startActivity(rIntent);
+            }
+        });
     }
 }
